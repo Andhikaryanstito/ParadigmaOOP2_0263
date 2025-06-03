96bc64a989db835585afafe1e10bf045e58510d4
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-06-03 10:31:31 +0700

    menambahkan class pekerja dab class budi

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -12,6 +12,17 @@ public:
     }
 };
 
+class pekerja : virtual public orang {
+public:
+
+   pekerja (int pUmur):
+   orang (pUmur)
+   {
+    cout <<"pelajar dibuat\n" <<endl;
+   }
+};
+
+
 class pelajar : virtual public orang {
 public:
 
@@ -20,4 +31,15 @@ public:
    {
     cout <<"pelajar dibuat\n" <<endl;
    }
+};
+
+class budi : public pekerja, public pelajar{
+    public:
+    budi (int pUmur) :
+    pekerja(pUmur),
+    pelajar(pUmur),
+    orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat \n" <<endl;
+    }
 };
