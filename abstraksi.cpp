ccc836616764221722db9f430345b06927bbdf44
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-06-03 10:40:42 +0700

    menampilkan nilai

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -14,4 +14,9 @@ class Abstraksiklas {
         x = a;
         y = b;
     }
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
 };
